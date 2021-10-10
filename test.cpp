c211a125199dd7ab7e07b17d24da51175db4417e
jirobassik <72316169+jirobassik@users.noreply.github.com>
2021-10-10 12:59:42 +0300

    Add all test

--- test.cpp
+++ test.cpp
@@ -1,10 +1,6 @@
 #include "pch.h"
 #include "../PPVIS/Realize.cpp"
 
-/*TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
-}*/
 
 
 ::testing::AssertionResult Sum1(bool foo3)
@@ -205,6 +201,70 @@
 }
 
 
+::testing::AssertionResult Delen1(bool foo3)
+{
+	Vector v1(20, 8, 9);
+	Vector v2(10, 2, 9);
+	Vector sr(2, 4, 1);
+	Vector v4;
+
+	v4 = v1 / v2;
+
+	if (v4 == sr)
+		return ::testing::AssertionSuccess();
+	else
+		return ::testing::AssertionFailure() << foo3;
+
+}
+
+::testing::AssertionResult Delen2(bool foo3)
+{
+	Vector v1(6, 25, 81);
+	Vector v2(2, 5, 9);
+	Vector sr(3, 5, 9);
+	Vector v4;
+
+	v4 = v1 / v2;
+
+	if (v4 == sr)
+		return ::testing::AssertionSuccess();
+	else
+		return ::testing::AssertionFailure() << foo3;
+
+}
+
+::testing::AssertionResult Delen3(bool foo3)
+{
+	Vector v1(45, 6, 12);
+	Vector v2(5, 2, 4);
+	Vector sr(9, 3, 3);
+	Vector v4;
+
+	v1 /= v2;
+
+	if (v1 == sr)
+		return ::testing::AssertionSuccess();
+	else
+		return ::testing::AssertionFailure() << foo3;
+
+}
+
+::testing::AssertionResult Delen4(bool foo3)
+{
+	Vector v1(14, 36, 7);
+	Vector v2(7, 6, 7);
+	Vector sr(2, 6, 1);
+	Vector v4;
+
+	v1 /= v2;
+
+	if (v1 == sr)
+		return ::testing::AssertionSuccess();
+	else
+		return ::testing::AssertionFailure() << foo3;
+
+}
+
 
 ::testing::AssertionResult ProizvCh1(bool foo3)
 {
@@ -264,6 +324,135 @@
 	v1 *= 9;
 
 	if (v1 == sr)
+		return ::testing::AssertionSuccess();
+	else
+		return ::testing::AssertionFailure() << foo3;
+
+}
+
+
+::testing::AssertionResult Cos1(bool foo3)
+{
+	Vector s1(2, 3, 1);
+	Vector s2(9, 2, 7);
+	Cos v4;
+	Cos sr(1);
+	 
+
+	v4 = s1^s2;
+
+	if (v4 == sr)
+		return ::testing::AssertionSuccess();
+	else
+		return ::testing::AssertionFailure() << foo3;
+
+}
+
+
+::testing::AssertionResult Cos2 (bool foo3)
+{
+	Vector s1(5, 9, 8);
+	Vector s2(13, 12, 17);
+	Cos v4;
+	Cos sr(1);
+
+
+	v4 = s1 ^ s2;
+
+	if (v4 == sr)
+		return ::testing::AssertionSuccess();
+	else
+		return ::testing::AssertionFailure() << foo3;
+
+}
+
+
+::testing::AssertionResult Cos3(bool foo3)
+{
+	Vector s1(4, 16, 20);
+	Vector s2(4, 7, 0);
+	Cos v4;
+	Cos sr(1);
+
+
+	v4 = s1 ^ s2;
+
+	if (v4 == sr)
+		return ::testing::AssertionSuccess();
+	else
+		return ::testing::AssertionFailure() << foo3;
+
+}
+
+
+::testing::AssertionResult Cos4(bool foo3)
+{
+	Vector s1(12, 15, 17);
+	Vector s2(9, 2, 7);
+	Cos v4;
+	Cos sr(1);
+
+
+	v4 = s1 ^ s2;
+
+	if (v4 == sr)
+		return ::testing::AssertionSuccess();
+	else
+		return ::testing::AssertionFailure() << foo3;
+
+}
+
+
+::testing::AssertionResult VecLen1(bool foo3)
+{
+	Vector v1(12, 15, 17);
+	
+	double sr = v1.length_v();
+
+
+	if (sr == 26)
+		return ::testing::AssertionSuccess();
+	else
+		return ::testing::AssertionFailure() << foo3;
+
+}
+
+::testing::AssertionResult VecLen2(bool foo3)
+{
+	Vector v1(4, 8, 16);
+
+	double sr = v1.length_v();
+
+
+	if (sr == 19)
+		return ::testing::AssertionSuccess();
+	else
+		return ::testing::AssertionFailure() << foo3;
+
+}
+
+::testing::AssertionResult VecLen3(bool foo3)
+{
+	Vector v1(3, 7, 5);
+
+	double sr = v1.length_v();
+
+
+	if (sr == 10)
+		return ::testing::AssertionSuccess();
+	else
+		return ::testing::AssertionFailure() << foo3;
+
+}
+
+::testing::AssertionResult VecLen4(bool foo3)
+{
+	Vector v1(13, 16, 2);
+
+	double sr = v1.length_v();
+
+
+	if (sr == 21)
 		return ::testing::AssertionSuccess();
 	else
 		return ::testing::AssertionFailure() << foo3;
@@ -354,4 +543,65 @@ TEST(ProizvChTest, Test4)
 	EXPECT_TRUE(ProizvCh4(true));
 }
 
+
+TEST(CosTest, Test1)
+{
+	EXPECT_TRUE(Cos1(true));
+}
+TEST(CosTest, Test2)
+{
+	EXPECT_TRUE(Cos2(true));
+}
+
+TEST(CosTest, Test3)
+{
+	EXPECT_TRUE(Cos3(true));
+}
+
+TEST(CosTest, Test4)
+{
+	EXPECT_TRUE(Cos4(true));
+}
+
+
+TEST(DelenTest, Test1)
+{
+	EXPECT_TRUE(Delen1(true));
+}
+
+TEST(DelenTest, Test2)
+{
+	EXPECT_TRUE(Delen2(true));
+}
+
+TEST(DelenTest, Test3)
+{
+	EXPECT_TRUE(Delen3(true));
+}
+
+TEST(DelenTest, Test4)
+{
+	EXPECT_TRUE(Delen4(true));
+}
+
+
+TEST(VecLenTest, Test1)
+{
+	EXPECT_TRUE(VecLen1(true));
+}
+
+TEST(VecLenTest, Test2)
+{
+	EXPECT_TRUE(VecLen2(true));
+}
+
+TEST(VecLenTest, Test3)
+{
+	EXPECT_TRUE(VecLen3(true));
+}
+
+TEST(VecLenTest, Test4)
+{
+	EXPECT_TRUE(VecLen4(true));
+}
 
