99f52d4303cb49d7ff76561600f8f783f8686412
jirobassik <72316169+jirobassik@users.noreply.github.com>
2021-10-18 18:58:36 +0300

    Small changes

--- PPVIS-2/Main.cpp
+++ PPVIS-2/Main.cpp
@@ -4,6 +4,8 @@
 
 int main()
 {
+	using namespace std;
+
 	Triangle ABC;
 
 	Right_triangle GPA;
@@ -16,19 +18,26 @@ int main()
 
 	Rectangle GPLN;
 
+	Rhombus PLKM;
+
+	
 
 	ABC.input();
 
 	GPA.input();
 
-	PPA.input();
+	Triangle* Shape = &PPA;
+
+	Shape->input();//PPA.input();
 
 	GLK.input();
 
 	ABCD.input();
 
 	GPLN.input();
-	
+
+	PLKM.input();
+
 
 	ABC.draw();
 
@@ -42,6 +51,8 @@ int main()
 
 	GPLN.draw();
 
+	PLKM.draw();
+
 /*
 cout << "\t\tFlat_shape\n";
 cout << "\t\t    |\n" <<
@@ -49,31 +60,31 @@ cout << "\t\t    |\n" <<
 		"\t\t    |\n" <<
 		"\t\t    |\n" <<
 		"\t\t    |\n" <<
-		"  -----------------------------------\n" <<
-		"  |\t\t\t\t|\n" <<
-		"  |\t\t\t\t|\n" <<
-		"  |\t\t\t\t|\n" <<
-		"  |\t\t\t\t|\n" <<
-		"  |\t\t\t\t|\n" <<
-		"  |\t\t\t\t|\n"; 
-           ABC.draw();
-cout << "  |---------1---------->|\t\n" <<
+		"  ----------------------------------------------------------------------------\n" <<
+		"  |\t\t\t\t|\t\t|\t\t|\n" <<
+		"  |\t\t\t\t|\t\t|\t\t|\n" <<
+		"  |\t\t\t\t|\t\t|\t\t|\n" <<
+		"  |\t\t\t\t|\t\t|\t\t|\n" <<
+		"  |\t\t\t\t|\t\t|\t\t|\n" <<
+		"  |\t\t\t\t|\t\t|\t\t|\n"; ABCD.draw2();
+ABC.draw2(); GPLN.draw2(); PLKM.draw2();
+cout << "  |\t\t\t|\t|\n" <<
 	    "  |\t\t\t|\t|\n" <<
 	    "  |\t\t\t|\t|\n" <<
 	    "  |\t\t\t|\t|\n" <<
-	    "  |<--------2------------------ |\n" <<
-	    "  |\t\t\t|\t|\n" ; GPA.draw();
-  cout<<"  |---------3---------->|\t\n" 
+	    "  |<--------2------------------ |\n" <<  
+	    "  |\t\t\t|\t|\n" ; GPA.draw2(); 
+  cout<<"  |---------3---------->|\t|\n" 
 	    "  |\t\t\t|\t|\n" <<
 	    "  |\t\t\t|\t|\n" <<
 	    "  |\t\t\t|\t|\n" <<
 	    "  |\t\t\t|\t|\n" <<
-	    "  |<-------1+2----------|<------|\n"; PPA.draw();
+	    "  |<-------1+2----------|<------|\n"; PPA.draw2();
 cout<<  "  |\t\t\t|\t|\n" <<
         "  |\t\t\t|\t|\n" <<
 	    "  |\t\t\t|\t|\n" <<
 	    "  |\t\t\t|\t|\n" <<
 	    "  |\t\t\t|\t|\n" <<
-	    "  |<-------1+2+3--------|<------|\n"; GLK.draw();
-		*/
+	    "  |<-------1+2+3--------|<------|\n"; GLK.draw2();
+*/		
 }
